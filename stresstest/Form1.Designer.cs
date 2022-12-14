f63caa5ac35fa3237d7482d989315931ef8e5398
Tommy Hagberg <tommy.hagberg@vgregion.se>
2022-12-14 12:27:46 +0100

    ny

---
+++ stresstest/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace stresstest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(375, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CONNECT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(375, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(287, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(287, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(375, 132);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(158, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(192, 262);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Stresstest";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(84, 309);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Analystyp från";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(195, 306);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(84, 357);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Analystyp till";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(195, 357);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(121, 21);
+            this.radioButton1.TabIndex = 10;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Analysis Blood";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 39);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(114, 21);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.Text = "Analysis DNA";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 66);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(126, 21);
+            this.radioButton3.TabIndex = 12;
+            this.radioButton3.Text = "Analysis Tomör";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(12, 93);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(115, 21);
+            this.radioButton4.TabIndex = 13;
+            this.radioButton4.Text = "Analysis FISH";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(12, 120);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(162, 21);
+            this.radioButton5.TabIndex = 14;
+            this.radioButton5.Text = "Analysis  Counselling";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(12, 147);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(136, 21);
+            this.radioButton6.TabIndex = 15;
+            this.radioButton6.Text = "Analysis Referral";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(12, 174);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(128, 21);
+            this.radioButton7.TabIndex = 16;
+            this.radioButton7.Text = "Analysis Foetus";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            this.radioButton7.CheckedChanged += new System.EventHandler(this.radioButton7_CheckedChanged);
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(12, 201);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(147, 21);
+            this.radioButton8.TabIndex = 17;
+            this.radioButton8.Text = "Analysis Fibroblast";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            this.radioButton8.CheckedChanged += new System.EventHandler(this.radioButton8_CheckedChanged);
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(12, 228);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(132, 21);
+            this.radioButton9.TabIndex = 18;
+            this.radioButton9.Text = "Analysis Amnion";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            this.radioButton9.CheckedChanged += new System.EventHandler(this.radioButton9_CheckedChanged);
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(12, 255);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(134, 21);
+            this.radioButton10.TabIndex = 19;
+            this.radioButton10.Text = "Analysis Chorion";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            this.radioButton10.CheckedChanged += new System.EventHandler(this.radioButton10_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(375, 303);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 50);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Stresstesta!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 383);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radioButton10);
+            this.Controls.Add(this.radioButton9);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ stresstest/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stresstest
+{
+   
+    public partial class Form1 : Form
+    {
+        String tabell;
+        SqlConnection cnn;
+        string connectionString;
+        int fran;
+        int till;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            textBox1.Update();
+            textBox2.Update();
+          //  textBox3.Update();
+         //   textBox4.Update();
+           
+         //   fran = Int16.Parse(textBox3.Text);
+           
+        //    till = Int16.Parse(textBox4.Text);
+            // connectionString = @"Data Source=Klingen-test-su-db,62468;Initial Catalog=Klingen_Test;User ID=tomha5;Password=202211";
+            connectionString = @"Data Source=Klingen-test-su-db,62468;Initial Catalog=Klingen_Test;User ID=" + textBox1.Text + ";Password=" + textBox2.Text +"";
+            //  connectionString = @"ODBC;DRIVER={SQL Server};UID=tomha5;PWD=202211;server = Klingen-test-su-db,62468;DATABASE = KlinGen_Test";
+            cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            MessageBox.Show("Connection Open  !");
+
+
+            //////////////////////////////////////////////////////////
+
+
+            //SqlCommand command;
+            //SqlDataReader dataReader;
+            //String Sql, Output = "";
+            //Sql = "sp_insert_enkel";
+            //command = new SqlCommand(Sql, cnn);
+            //command.CommandType = CommandType.StoredProcedure;
+
+
+            ////command.Parameters.Add(new SqlParameter("@tabell", "Analysis Blood"));
+            ////command.Parameters.Add(new SqlParameter("@analystyp", 1000));
+
+            //command.Parameters.Add(new SqlParameter("@tabell",tabell));
+            //command.Parameters.Add(new SqlParameter("@analystyp", 1));
+            //command.Parameters.Add(new SqlParameter("@patient", 12171));
+            //command.Parameters.Add(new SqlParameter("@Signature", textBox1.Text));
+            //command.Parameters.Add(new SqlParameter("@SubmitterName", "GB"));
+            //command.Parameters.Add(new SqlParameter("@Submitter", "xx"));
+            //command.Parameters.Add(new SqlParameter("@Type", 1));
+            //command.Parameters.Add(new SqlParameter("@Urgent", 1));
+            //command.Parameters.Add(new SqlParameter("@Arriveddate_s", "20121001"));
+            //command.Parameters.Add(new SqlParameter("@InvoiceDate_s", "20121002"));
+            //command.Parameters.Add(new SqlParameter("@Indication", 1));
+            //command.Parameters.Add(new SqlParameter("@Research", 1));
+            //command.Parameters.Add(new SqlParameter("@Savedmaterial", 1));
+            //command.Parameters.Add(new SqlParameter("@Canister", "xxx"));
+            //command.Parameters.Add(new SqlParameter("@Box", "fg"));
+            //command.Parameters.Add(new SqlParameter("@Number", "123"));
+            //command.Parameters.Add(new SqlParameter("@Remark", "yyyy"));
+            //command.Parameters.Add(new SqlParameter("@OrdererInternal", 1));
+            //command.Parameters.Add(new SqlParameter("@Orderer", 1));
+            //command.Parameters.Add(new SqlParameter("@AccountableInternal", 1));
+            //command.Parameters.Add(new SqlParameter("@Accountable", 1));
+            //command.Parameters.Add(new SqlParameter("@Result", 7));
+            //command.Parameters.Add(new SqlParameter("@Quality", 8));
+            //command.Parameters.Add(new SqlParameter("@Diagnosis", "fel"));
+            //command.Parameters.Add(new SqlParameter("@McKusick_s", "12"));
+            //command.Parameters.Add(new SqlParameter("@Answered", 1));
+            //command.Parameters.Add(new SqlParameter("@Price_s", "1000"));
+            //command.Parameters.Add(new SqlParameter("@AnsweredDate_s", "20121004"));
+            //command.Parameters.Add(new SqlParameter("@InvoiceNr", 10));
+            //command.Parameters.Add(new SqlParameter("@Doctor", 11));
+            //command.Parameters.Add(new SqlParameter("@Metaphase", "yyy"));
+            //command.Parameters.Add(new SqlParameter("@Locked", 1));
+            //command.Parameters.Add(new SqlParameter("@Change", "zzz"));
+            //command.Parameters.Add(new SqlParameter("@ResultInternal", "tt"));
+            //command.Parameters.Add(new SqlParameter("@Prenatal", 1));
+            //command.Parameters.Add(new SqlParameter("@ResultInternal2", 100));
+            //command.Parameters.Add(new SqlParameter("@Family", 200));
+            //command.Parameters.Add(new SqlParameter("@CareDate_s", "20121003"));
+            //command.Parameters.Add(new SqlParameter("@SpecimenType", "xxx"));
+
+
+            //dataReader = command.ExecuteReader();
+            //dataReader.Close();
+            //  for (int a = fran; a <= till; a++)
+            //   {
+            //    // SqlDataReader dataReader2;
+            //   command.Parameters.RemoveAt(1);
+            //    //    command.Parameters.Add(new SqlParameter("@analystyp", a));
+            //    command.Parameters.Insert(1, new SqlParameter("@analystyp", a));
+            //       dataReader = command.ExecuteReader();
+            //    if(a < till)
+            //       dataReader.Close();
+            //}
+            //while (dataReader.Read())
+            //{
+            //    Output = Output + dataReader.GetValue(0) + " - " + dataReader.GetValue(1);
+
+            //}
+            //MessageBox.Show(Output);
+
+
+            //cnn.Close();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          //  textBox1.Update();
+           // textBox1.valu
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            tabell = "dbo_Analysis Blood";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            tabell = "dbo_Analysis DNA";
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            tabell = "dbo_Analysis Tumor";
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            tabell = "dbo_Analysis FISH";
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            tabell = "dbo_Analysis Counselling";
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            tabell = "dbo_Analysis Referral";
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+            tabell = "dbo_Analysis Foetus";
+        }
+
+        private void radioButton8_CheckedChanged(object sender, EventArgs e)
+        {
+            tabell = "dbo_Analysis Fibroblast";
+        }
+
+        private void radioButton9_CheckedChanged(object sender, EventArgs e)
+        {
+            tabell = "dbo_Analysis Amnion";
+        }
+
+        private void radioButton10_CheckedChanged(object sender, EventArgs e)
+        {
+            tabell = "dbo_Analysis Chorion";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand command;
+            SqlDataReader dataReader;
+            textBox3.Update();
+            textBox4.Update();
+
+            fran = Int16.Parse(textBox3.Text);
+
+            till = Int16.Parse(textBox4.Text);
+            String Sql, Output = "";
+            Sql = "sp_insert_enkel";
+            command = new SqlCommand(Sql, cnn);
+            command.CommandType = CommandType.StoredProcedure;
+
+
+            //command.Parameters.Add(new SqlParameter("@tabell", "Analysis Blood"));
+            //command.Parameters.Add(new SqlParameter("@analystyp", 1000));
+            String datum;
+            datum = DateTime.Now.ToString("MMMM dd, yyyy");
+
+            command.Parameters.Add(new SqlParameter("@tabell", tabell));
+            command.Parameters.Add(new SqlParameter("@analystyp", 1));
+            command.Parameters.Add(new SqlParameter("@patient", 12171));
+            command.Parameters.Add(new SqlParameter("@Signature", textBox1.Text));
+            command.Parameters.Add(new SqlParameter("@SubmitterName", "GB"));
+            command.Parameters.Add(new SqlParameter("@Submitter", "xx"));
+            command.Parameters.Add(new SqlParameter("@Type", 1));
+            command.Parameters.Add(new SqlParameter("@Urgent", 1));
+            command.Parameters.Add(new SqlParameter("@Arriveddate_s", "20121001"));
+            command.Parameters.Add(new SqlParameter("@InvoiceDate_s", "20121002"));
+            command.Parameters.Add(new SqlParameter("@Indication", 1));
+            command.Parameters.Add(new SqlParameter("@Research", 1));
+            command.Parameters.Add(new SqlParameter("@Savedmaterial", 1));
+            command.Parameters.Add(new SqlParameter("@Canister", "xxx"));
+            command.Parameters.Add(new SqlParameter("@Box", "fg"));
+            command.Parameters.Add(new SqlParameter("@Number", "123"));
+            command.Parameters.Add(new SqlParameter("@Remark", "yyyy"));
+            command.Parameters.Add(new SqlParameter("@OrdererInternal", 1));
+            command.Parameters.Add(new SqlParameter("@Orderer", 1));
+            command.Parameters.Add(new SqlParameter("@AccountableInternal", 1));
+            command.Parameters.Add(new SqlParameter("@Accountable", 1));
+            command.Parameters.Add(new SqlParameter("@Result", 7));
+            command.Parameters.Add(new SqlParameter("@Quality", 8));
+            command.Parameters.Add(new SqlParameter("@Diagnosis", "fel"));
+            command.Parameters.Add(new SqlParameter("@McKusick_s", "12"));
+            command.Parameters.Add(new SqlParameter("@Answered", 1));
+            command.Parameters.Add(new SqlParameter("@Price_s", "1000"));
+            command.Parameters.Add(new SqlParameter("@AnsweredDate_s", datum));
+            command.Parameters.Add(new SqlParameter("@InvoiceNr", 10));
+            command.Parameters.Add(new SqlParameter("@Doctor", 11));
+            command.Parameters.Add(new SqlParameter("@Metaphase", "yyy"));
+            command.Parameters.Add(new SqlParameter("@Locked", 1));
+            command.Parameters.Add(new SqlParameter("@Change", "zzz"));
+            command.Parameters.Add(new SqlParameter("@ResultInternal", "tt"));
+            command.Parameters.Add(new SqlParameter("@Prenatal", 1));
+            command.Parameters.Add(new SqlParameter("@ResultInternal2", 100));
+            command.Parameters.Add(new SqlParameter("@Family", 200));
+            command.Parameters.Add(new SqlParameter("@CareDate_s", "20121003"));
+            command.Parameters.Add(new SqlParameter("@SpecimenType", "xxx"));
+
+
+            dataReader = command.ExecuteReader();
+            dataReader.Close();
+            for (int a = fran; a <= till; a++)
+            {
+                // SqlDataReader dataReader2;
+                command.Parameters.RemoveAt(1);
+                //    command.Parameters.Add(new SqlParameter("@analystyp", a));
+                command.Parameters.Insert(1, new SqlParameter("@analystyp", a));
+                dataReader = command.ExecuteReader();
+                if (a < till)
+                    dataReader.Close();
+            }
+            while (dataReader.Read())
+            {
+                Output = Output + dataReader.GetValue(0) + " - " + dataReader.GetValue(1);
+
+            }
+            MessageBox.Show(Output);
+
+
+            cnn.Close();
+
+        }
+    }
+}
